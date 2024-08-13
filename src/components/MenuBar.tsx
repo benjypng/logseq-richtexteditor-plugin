ddf7e3c6f380028fe7be70095b93605f49a095ee
Benjy <ben@pngs.cc>
2024-08-13 12:31:55 +0800

    feat: dynamic menu bar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -4,6 +4,7 @@ export const MenuBar = ({ editor }: { editor: Editor }) => {
   if (!editor) {
     return null
   }
+  console.log(editor.isActive('heading'))
 
   return (
     <div className="rte-control-group">
@@ -12,12 +13,18 @@ export const MenuBar = ({ editor }: { editor: Editor }) => {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 1 }).run()
           }
+          className={
+            editor.isActive('heading', { level: 1 }) ? 'is-active' : ''
+          }
         >
           H1
         </button>
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={
+            editor.isActive('heading', { level: 2 }) ? 'is-active' : ''
           }
         >
           H2
@@ -26,41 +33,67 @@ export const MenuBar = ({ editor }: { editor: Editor }) => {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 3 }).run()
           }
+          className={
+            editor.isActive('heading', { level: 3 }) ? 'is-active' : ''
+          }
         >
           H3
         </button>
-        <button onClick={() => editor.chain().focus().setParagraph().run()}>
+        <button
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={editor.isActive('paragraph') ? 'is-active' : ''}
+        >
           Paragraph
         </button>
-        <button onClick={() => editor.chain().focus().toggleBold().run()}>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
           Bold
         </button>
-        <button onClick={() => editor.chain().focus().toggleItalic().run()}>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive('italic') ? 'is-active' : ''}
+        >
           Italic
         </button>
-        <button onClick={() => editor.chain().focus().toggleStrike().run()}>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={editor.isActive('strike') ? 'is-active' : ''}
+        >
           Strike
         </button>
-        <button onClick={() => editor.chain().focus().toggleHighlight().run()}>
+        <button
+          onClick={() => editor.chain().focus().toggleHighlight().run()}
+          className={editor.isActive('highlight') ? 'is-active' : ''}
+        >
           Highlight
         </button>
         <button
           onClick={() => editor.chain().focus().setTextAlign('left').run()}
+          className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}
         >
           Left
         </button>
         <button
           onClick={() => editor.chain().focus().setTextAlign('center').run()}
+          className={
+            editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''
+          }
         >
           Center
         </button>
         <button
           onClick={() => editor.chain().focus().setTextAlign('right').run()}
+          className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}
         >
           Right
         </button>
         <button
           onClick={() => editor.chain().focus().setTextAlign('justify').run()}
+          className={
+            editor.isActive({ textAlign: 'justify' }) ? 'is-active' : ''
+          }
         >
           Justify
         </button>
